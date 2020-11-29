3d841fdc064597f3fc551931e7c14aa698b81c76
Per Lindgren <per.lindgren@ltu.se>
2020-11-29 13:35:04 +0100

    wip, docs

--- src/data/usb_midi/usb_midi_event_packet.rs
+++ src/data/usb_midi/usb_midi_event_packet.rs
@@ -1,9 +1,8 @@
+use crate::data::byte::u4::U4;
+use crate::data::midi::message::raw::{Payload, Raw};
+use crate::data::midi::message::Message;
 use crate::data::usb_midi::cable_number::CableNumber;
 use crate::data::usb_midi::code_index_number::CodeIndexNumber;
-use crate::data::midi::message::Message;
-use crate::data::byte::u4::U4;
-use crate::data::midi::message::raw::{Payload,Raw};
-
 
 /// A packet that communicates with the host
 /// Currently supported is sending the specified normal midi
@@ -18,42 +17,37 @@ use crate::data::midi::message::raw::{Payload,Raw};
 /// See USB Device Class Definition for MIDI Devices, Section 4
 ///
 pub struct UsbMidiEventPacket {
-    pub cable_number : CableNumber,
-    pub message: Message
+    pub cable_number: CableNumber,
+    pub message: Message,
 }
 
-impl From<UsbMidiEventPacket> for [u8;4] {
-    fn from(value:UsbMidiEventPacket) -> [u8;4] {
-        let message= value.message;
+impl From<UsbMidiEventPacket> for [u8; 4] {
+    fn from(value: UsbMidiEventPacket) -> [u8; 4] {
+        let message = value.message;
         let cable_number = U4::from(value.cable_number);
         let index_number = {
-                let code_index = 
-                        CodeIndexNumber::find_from_message(&message);
-                U4::from(code_index)
+            let code_index = CodeIndexNumber::find_from_message(&message);
+            U4::from(code_index)
         };
         // Byte 0
-        let header = U4::combine(cable_number,index_number);
+        let header = U4::combine(cable_number, index_number);
 
         let raw_midi = Raw::from(message);
         let status = raw_midi.status;
 
         match raw_midi.payload {
-            Payload::Empty => [header,status,0,0],
-            Payload::SingleByte(byte) => 
-                                [header,status,byte.into(),0],
-            Payload::DoubleByte(byte1,byte2) => 
-                                    [header,status,byte1.into(),byte2.into()]           
+            Payload::Empty => [header, status, 0, 0],
+            Payload::SingleByte(byte) => [header, status, byte.into(), 0],
+            Payload::DoubleByte(byte1, byte2) => [header, status, byte1.into(), byte2.into()],
         }
     }
 }
 
-impl UsbMidiEventPacket{
-
-    pub fn from_midi(cable:CableNumber, midi:Message)
-        -> UsbMidiEventPacket{
-        UsbMidiEventPacket{
-            cable_number : cable,
-            message : midi
+impl UsbMidiEventPacket {
+    pub fn from_midi(cable: CableNumber, midi: Message) -> UsbMidiEventPacket {
+        UsbMidiEventPacket {
+            cable_number: cable,
+            message: midi,
         }
     }
 }
